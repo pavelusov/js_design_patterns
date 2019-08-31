99572bd5038411adbda13b0dbb73fa6a3c0b294e
Pavel Usov <ipavelusov@gmail.com>
2019-08-31 17:08:33 +0500

    Use the Module Reveal pattern

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -1,45 +1,51 @@
 "use strict";
-var chatModule = (function () {
-  var leadself = "Me: ",
-    leadcomputer = "PC: ",
-    aSaid = ["This is a Cyber Chat"],
-    msgYes = "Yes, that's a great idea.",
-    msgNo = "No, that must be a mistake.",
-    aSassyStuff = ["Like mold on books, grow myths on history.",
+const chatModule = (function () {
+  const _leadself = "Me: ",
+    _leadcomputer = "PC: ",
+    _aSaid = ["This is a Cyber Chat"],
+    _msgYes = "Yes, that's a great idea.",
+    _msgNo = "No, that must be a mistake.",
+    _aSassyStuff = ["Like mold on books, grow myths on history.",
       "She moved like a poem and smiled like a sphinx.",
       "As long as we don’t die, this is gonna be one hell of a story.",
       "She laughed, and the desert sang.",
       "You’ve got about as much charm as a dead slug."];
 
-  function echo(msg) {
-    aSaid.push('<div>' + msg + '</div>');
+  function _echo(msg) {
+    _aSaid.push('<div>' + msg + '</div>');
 
-    var MAX_ROW_VALUE = 6;
-    var aSaidLength = aSaid.length;
-    var start = Math.max(aSaidLength - MAX_ROW_VALUE, 0);
-    var out = '';
+    const MAX_ROW_VALUE = 6;
+    const aSaidLength = _aSaid.length;
+    const start = Math.max(aSaidLength - MAX_ROW_VALUE, 0);
+    let out = '';
 
-    for (var i = start; i < aSaidLength; i += 1) {
-      out += aSaid[i];
+    for (let i = start; i < aSaidLength; i += 1) {
+      out += _aSaid[i];
     }
 
     $('.advert').html(out);
     $('#talk span').text(msg);
   }
 
+  function talk(msg) {
+    _echo(_leadself + msg);
+  }
+
+  function replayYesNo() {
+    const msg = Math.random() > .5 ? _msgYes : _msgNo;
+    _echo(_leadcomputer + msg);
+  }
+
+  function saySassyStuff() {
+    const index = Math.floor(Math.random() * _aSassyStuff.length);
+    const msg = _aSassyStuff[index];
+    _echo(_leadcomputer + msg);
+  }
+
   return {
-    talk: function (msg) {
-      echo(leadself + msg);
-    },
-    replayYesNo: function () {
-      var msg = Math.random() > .5 ? msgYes : msgNo;
-      echo(leadcomputer + msg);
-    },
-    saySassyStuff: function saySassyStuff() {
-      var index = Math.floor(Math.random() * aSassyStuff.length);
-      var msg = aSassyStuff[index];
-      echo(leadcomputer + msg);
-    }
+    talk,
+    replayYesNo,
+    saySassyStuff,
   };
 })();
 
