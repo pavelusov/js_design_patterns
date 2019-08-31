fb7be25a785f880d9d60eceee8237324b8a1a0a4
Pavel Usov <ipavelusov@gmail.com>
2019-08-31 16:43:55 +0500

    Use the Module Design Pattern. Hidden privet variables and gave access to the public side

--- section1/start/js/script.js
+++ section1/start/js/script.js
@@ -1,44 +1,52 @@
 "use strict";
-var com = com || {};
-com.github = com.github || {};
-com.github.JSDP = com.github.JSDP || {};
+var chatModule = (function () {
+  var leadself = "Me: ",
+    leadcomputer = "PC: ",
+    aSaid = ["This is a Cyber Chat"],
+    msgYes = "Yes, that's a great idea.",
+    msgNo = "No, that must be a mistake.",
+    aSassyStuff = ["Like mold on books, grow myths on history.",
+      "She moved like a poem and smiled like a sphinx.",
+      "As long as we don’t die, this is gonna be one hell of a story.",
+      "She laughed, and the desert sang.",
+      "You’ve got about as much charm as a dead slug."];
 
-com.github.JSDP.section1 = {
-  leadself: "Me: ",
-  leadcomputer: "PC: ",
-  aSaid: ["This is a Cyber Chat"],
-  msgYes: "Yes, that's a great idea.",
-  msgNo: "No, that must be a mistake.",
-  aSassyStuff: ["Like mold on books, grow myths on history.",
-    "She moved like a poem and smiled like a sphinx.",
-    "As long as we don’t die, this is gonna be one hell of a story.",
-    "She laughed, and the desert sang.",
-    "You’ve got about as much charm as a dead slug."],
-  talk: function (msg) {
-    this.echo(this.leadself + msg);
-  },
-  replayYesNo: function () {
-    var msg = Math.random() > .5 ? this.msgYes : this.msgNo;
-    this.echo(this.leadcomputer + msg);
-  },
-  saySassyStuff: function saySassyStuff() {
-    var index = Math.floor(Math.random() * this.aSassyStuff.length);
-    var msg = this.aSassyStuff[index];
-    this.echo(this.leadcomputer + msg);
-  },
-  echo: function (msg) {
-    this.aSaid.push('<div>' + msg + '</div>');
+  function echo(msg) {
+    aSaid.push('<div>' + msg + '</div>');
 
     var MAX_ROW_VALUE = 6;
-    var aSaidLength = this.aSaid.length;
+    var aSaidLength = aSaid.length;
     var start = Math.max(aSaidLength - MAX_ROW_VALUE, 0);
     var out = '';
 
     for (var i = start; i < aSaidLength; i += 1) {
-      out += this.aSaid[i];
+      out += aSaid[i];
     }
 
     $('.advert').html(out);
     $('#talk span').text(msg);
   }
-};
+
+  return {
+    talk: function (msg) {
+      echo(leadself + msg);
+    },
+    replayYesNo: function () {
+      var msg = Math.random() > .5 ? msgYes : msgNo;
+      echo(leadcomputer + msg);
+    },
+    saySassyStuff: function saySassyStuff() {
+      var index = Math.floor(Math.random() * aSassyStuff.length);
+      var msg = aSassyStuff[index];
+      echo(leadcomputer + msg);
+    }
+  };
+})();
+
+$(function () {
+  chatModule.talk('hello');
+  chatModule.replayYesNo();
+  chatModule.saySassyStuff();
+  chatModule.saySassyStuff();
+  chatModule.saySassyStuff();
+});
