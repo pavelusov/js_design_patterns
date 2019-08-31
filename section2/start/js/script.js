77bc7e6973e306f88a25f09a7b35e3ce6d11829e
Pavel Usov <ipavelusov@gmail.com>
2019-08-31 19:13:07 +0500

    Use the Singletone pattern

--- section2/start/js/script.js
+++ section2/start/js/script.js
@@ -1,12 +1,58 @@
 (function(win, $){
-	$(win.document).ready(function(){
-		$('.advert').click(function(e){
-			var circle = $('<div class="circle"></div>');
-				circle.css('left',e.pageX-25);
-				circle.css('top',e.pageY-25)
-			$('.advert').append(circle);
+	const CircleGeneratorSingleton = (function () {
+		let instance;
+
+		function _init() {
+
+			function _setPosition(circle, left, right) {
+					circle.css('left', left);
+					circle.css('top', right)
+			}
+
+			function create(left, right) {
+				const circle = $('<div class="circle"></div>');
+				_setPosition(circle, left, right);
+				return circle;
+			}
+
+			function add(circle) {
+				$('.advert').append(circle);
+			}
+
+			return {
+				create,
+				add,
+			}
+		}
+
+		function getInstance() {
+			if (!instance) {
+				instance = _init();
+			}
+			return instance;
+		}
+
+		return {
+			getInstance,
+		}
+	})();
+	$(function(){
+		$('.advert').on('click', function(e){
+			const cg = CircleGeneratorSingleton.getInstance();
+			const circle = cg.create(e.clientX, e.clientY);
+			cg.add(circle);
 		});
 
+		document.addEventListener('keypress', function (e) {
+			if (e.key === 'a') {
+				const cg = CircleGeneratorSingleton.getInstance();
+				const circle = cg.create(
+					Math.random() * 600,
+					Math.random() * 600,
+				);
+				cg.add(circle);
+			}
+		});
 	});
 
 })(window, jQuery);
